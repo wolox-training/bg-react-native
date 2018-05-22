89c1cfeea16e3fc4a2c9b4914043e5df1dc1c6e5
Brian Gerez <briangerez93@gmail.com>
2018-05-22 14:28:50 -0300

    Made up to 'Lifting State Up' inclusive

---
+++ react-main/first-app/src/app/components/App/Board/index.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+import Square from '../Square';
+
+class Board extends Component {
+  state = { squares: Array(9).fill(null) };
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({ squares });
+  }
+
+  renderSquare = i => <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Board;
