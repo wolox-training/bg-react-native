f737eae77a28808627390fb2d68ecec9464f4e2d
Brian Gerez <briangerez93@gmail.com>
2018-05-22 14:41:56 -0300

    Functional Components

--- react-main/first-app/src/app/components/App/Square/index.js
+++ react-main/first-app/src/app/components/App/Square/index.js
@@ -1,15 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import './styles.css';
 
-class Square extends Component {
-  render() {
-    return (
-      <button className="square" onClick={() => this.props.onClick()}>
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 export default Square;
