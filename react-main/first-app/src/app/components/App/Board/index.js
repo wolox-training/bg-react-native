0ba68b594559cefcfe003fb287cbaa65c89167da
Brian Gerez <briangerez93@gmail.com>
2018-05-22 16:46:26 -0300

    Storing a History

--- react-main/first-app/src/app/components/App/Board/index.js
+++ react-main/first-app/src/app/components/App/Board/index.js
@@ -3,28 +3,11 @@ import React, { Component } from 'react';
 import Square from '../Square';
 
 class Board extends Component {
-  state = { squares: Array(9).fill(null), xIsNext: true };
-
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({ squares : squares, xIsNext: !this.state.xIsNext });
-  }
-
-  renderSquare = i => <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
+  renderSquare = i => <Square value={this.props.squares[i]} onClick={() => this.props.onClick(i)} />;
 
   render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -46,23 +29,3 @@ class Board extends Component {
 }
 
 export default Board;
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
